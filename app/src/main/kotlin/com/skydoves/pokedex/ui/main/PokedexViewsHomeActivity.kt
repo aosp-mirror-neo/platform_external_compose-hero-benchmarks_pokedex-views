3b1e7d8616ac73de8b566bbcb60e29c473f2473e
Jossi Wolf <jossiwolf@google.com>
2025-09-12 20:57:23 +0100

    Improve PokedexTransitionBenchmark
    - Adds a trace block for the transition
    - Improves accuracy of transition status text by overriding onEnterAnimationComplete
    
    Test: PokedexTransitionBenchmark on Pixel 4A + verified results and trace blocks showing up in traces
    Change-Id: If73c08df00e4d562de1353b7956fe10a586fcdef

--- app/src/main/kotlin/com/skydoves/pokedex/ui/main/PokedexViewsHomeActivity.kt
+++ app/src/main/kotlin/com/skydoves/pokedex/ui/main/PokedexViewsHomeActivity.kt
@@ -17,6 +17,8 @@
 package com.skydoves.pokedex.ui.main
 
 import android.os.Bundle
+import android.transition.Transition
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -25,6 +27,7 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.tracing.Trace
 import androidx.tracing.trace
 import com.skydoves.baserecyclerviewadapter.RecyclerViewPaginator
 import com.skydoves.pokedex.R
@@ -36,9 +39,11 @@ import com.skydoves.pokedex.core.di.ModuleLocator
 import com.skydoves.pokedex.core.model.Pokemon
 import com.skydoves.pokedex.databinding.ActivityMainBinding
 import com.skydoves.pokedex.ui.details.DetailActivity
+import com.skydoves.pokedex.ui.details.DetailActivity.Companion.NAVIGATION_TRANSITION_TRACE_NAME
 import com.skydoves.pokedex.utils.requireBooleanExtra
 import com.skydoves.transformationlayout.TransformationLayout
 import com.skydoves.transformationlayout.onTransformationStartContainer
+import kotlin.random.Random
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.launch
 
@@ -52,39 +57,50 @@ class PokedexViewsHomeActivity : AppCompatActivity(R.layout.activity_main) {
 
     private lateinit var adapter: PokemonAdapter
 
+    /**
+     * Cookie for the trace block of the home <-> details transition. Should be set when starting a
+     * transition and reset when it ends.
+     */
+    private var transitionTraceCookie = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         trace("PokedexActivity Setup") {
             PokedexFeatureFlags.EnableTransformationLayout =
                 intent.requireBooleanExtra(POKEDEX_ENABLE_TRANSFORMATION_LAYOUT)
             PokedexFeatureFlags.EnableSharedElementTransitions =
                 intent.requireBooleanExtra(POKEDEX_ENABLE_SHARED_ELEMENT_TRANSITIONS)
-        }
-        // onTransformationStartContainer sets up activity transitions and needs to run before
-        //  the super's onCreate
-        if (PokedexFeatureFlags.EnableSharedElementTransitions) {
-            onTransformationStartContainer()
+            if (PokedexFeatureFlags.EnableSharedElementTransitions) {
+                // onTransformationStartContainer sets up activity transitions and needs to run
+                // before
+                //  the super's onCreate
+                onTransformationStartContainer()
+                // Set window transitions listeners only after onTransformationStartContainer
+                // initializes
+                //  them
+                setupSharedElementTransitionListeners()
+            }
         }
         super.onCreate(savedInstanceState)
 
         trace("ModuleLocator#attach") { ModuleLocator.attach { application } }
-        // Set window transitions listeners only after onTransformationStartContainer initializes
-        //  them
-        if (PokedexFeatureFlags.EnableSharedElementTransitions) {
-            setupSharedElementTransitionListeners()
-        }
         val onItemClicked: (Pokemon, TransformationLayout?) -> Unit =
             if (PokedexFeatureFlags.EnableSharedElementTransitions) {
                 { pokemon, transformationLayout ->
+                    transitionTraceCookie = startDetailActivityTransitionTrace()
                     DetailActivity.startActivityWithTransition(
                         transformationLayout =
                             requireNotNull(transformationLayout) {
                                 "No TransformationLayout instance was passed back from PokedexAdapter"
                             },
                         pokemon = pokemon,
+                        traceCookie = transitionTraceCookie,
                     )
                 }
             } else {
-                { pokemon, _ -> DetailActivity.startActivity(this, pokemon) }
+                { pokemon, _ ->
+                    transitionTraceCookie = startDetailActivityTransitionTrace()
+                    DetailActivity.startActivity(this, pokemon, traceCookie = transitionTraceCookie)
+                }
             }
         adapter = PokemonAdapter(onItemClicked = onItemClicked)
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
@@ -94,27 +110,57 @@ class PokedexViewsHomeActivity : AppCompatActivity(R.layout.activity_main) {
         observeViewModel()
     }
 
+    /**
+     * Start an async trace block with a new cookie for the details transition.
+     *
+     * @return The cookie for the trace block.
+     */
+    private fun startDetailActivityTransitionTrace(traceCookie: Int = Random.nextInt()): Int {
+        Trace.beginAsyncSection(NAVIGATION_TRANSITION_TRACE_NAME, traceCookie)
+        return traceCookie
+    }
+
+    private fun endDetailsTransitionTrace() {
+        if (transitionTraceCookie != -1) {
+            Trace.endAsyncSection(NAVIGATION_TRANSITION_TRACE_NAME, transitionTraceCookie)
+            transitionTraceCookie = -1
+        }
+    }
+
+    override fun onEnterAnimationComplete() {
+        Log.d("PokedexViewsHomeActivity", "onEnterAnimationComplete")
+        // onEnterAnimationComplete can get called multiple times when we have more than one
+        //  transition, which is the case for shared element. For shared element, we rely on the
+        //  transition listeners to update the status instead.
+        if (!PokedexFeatureFlags.EnableSharedElementTransitions) {
+            endDetailsTransitionTrace()
+            activityMainBinding.pokedexHomeTransitionStatus.text = HOME_TRANSITION_INACTIVE
+        }
+    }
+
     private fun setupSharedElementTransitionListeners() {
         val sharedElementTransitionListener =
-            object : android.transition.Transition.TransitionListener {
-                override fun onTransitionStart(transition: android.transition.Transition?) {
-                    activityMainBinding.pokedexHomeTransitionStatus.text =
-                        "pokedex-home-transition-active-true"
+            object : Transition.TransitionListener {
+                override fun onTransitionStart(transition: Transition) {
+                    Log.d("PokedexViewsHomeActivity", "onTransitionStart $transition")
+                    activityMainBinding.pokedexHomeTransitionStatus.text = HOME_TRANSITION_ACTIVE
                 }
 
-                override fun onTransitionEnd(transition: android.transition.Transition?) {
-                    activityMainBinding.pokedexHomeTransitionStatus.text =
-                        "pokedex-home-transition-active-false"
+                override fun onTransitionEnd(transition: Transition) {
+                    Log.d("PokedexViewsHomeActivity", "onTransitionEnd $transition")
+                    activityMainBinding.pokedexHomeTransitionStatus.text = HOME_TRANSITION_INACTIVE
+                    endDetailsTransitionTrace()
                 }
 
-                override fun onTransitionCancel(transition: android.transition.Transition?) {}
+                override fun onTransitionCancel(transition: Transition?) {}
 
-                override fun onTransitionPause(transition: android.transition.Transition?) {}
+                override fun onTransitionPause(transition: Transition?) {}
 
-                override fun onTransitionResume(transition: android.transition.Transition?) {}
+                override fun onTransitionResume(transition: Transition?) {}
             }
-        window.sharedElementExitTransition.addListener(sharedElementTransitionListener)
-        window.sharedElementReturnTransition.addListener(sharedElementTransitionListener)
+
+        window.sharedElementReturnTransition?.addListener(sharedElementTransitionListener)
+        window.sharedElementExitTransition?.addListener(sharedElementTransitionListener)
     }
 
     private fun setupRecyclerView() {
@@ -163,3 +209,6 @@ class PokedexViewsHomeActivity : AppCompatActivity(R.layout.activity_main) {
         ModuleLocator.detach()
     }
 }
+
+private const val HOME_TRANSITION_ACTIVE = "pokedex-home-transition-active-true"
+private const val HOME_TRANSITION_INACTIVE = "pokedex-home-transition-active-false"
